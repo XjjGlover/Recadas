aef005b6b945c9d639ff9c1a843a9788beef7d57
xujiajun <924207089@qq.com>
2020-07-02 21:08:30 +0800

    Add identity_sql to config.

--- case/login.py
+++ case/login.py
@@ -19,6 +19,7 @@ from util.handle_assert import do_assert
 from util.get_data import GetToken
 from util.handle_mysql import do_mysql
 from util.handle_log import do_log
+from util.handle_conf import do_conf
 
 
 @ddt
@@ -30,22 +31,22 @@ class TestLogin(unittest.TestCase):
     send_request = HttpRequest()
 
     @classmethod
-    def setUp(self):
+    def setUp(cls):
         """
         Gain the captcha and token for logining.
         :return:
         """
-        captcha_url = 'http://119.23.49.157/vmp/common/captcha/string'
-        res = self.send_request(method="GET", url=captcha_url, data={})
-        self.captcha = eval(res.text)["data"]["captcha"]
+        captcha_url = do_conf.get_value("url", "captcha_url")
+        res = cls.send_request(method="GET", url=captcha_url, data={})
+        cls.captcha = eval(res.text)["data"]["captcha"]
 
     @classmethod
-    def tearDown(self):
+    def tearDownClass(cls):
         """
         Close HTTP link after cases executed.
         :return:
         """
-        self.send_request.close()
+        cls.send_request.close()
         do_mysql.close()
 
     @data(*cases)
@@ -63,11 +64,9 @@ class TestLogin(unittest.TestCase):
         # Replace expected data from database,then gain expected data
         if do_re.matchdata(value.temp):
             user = eval(value.data)["username"]
-            sql = "SELECT t2.org_name as company_name,t.company_no,t1.id as user_id,t.org_name,t1.real_name,t3.id as role_id,t3.role_name,t3.role_type\
-                FROM t_ascs_vmp_org t,t_ascs_vmp_user t1,t_ascs_vmp_org t2,t_ascs_vmp_role t3,t_ascs_vmp_account_role_relationship t4\
-                WHERE t.id = t1.org_id and t.id = t2.id and t2.parent_org_id = 0 and t.id = t3.company_id and t.id = t4.company_id and t4.account_id = t1.id and t4.role_id = t3.id and t1.company_no = 'csb' and t1.username = %s;"
-            expect_data = dict(zip_longest(["companyName", "companyNo", "id", "orgName", "realName", "roleId", "roleName",
-                                       "roleType", "token"], do_mysql(sql, user)[0]))
+            sql = do_conf.get_value("sql", "identity_sql")
+            expect_data = dict(zip_longest(["companyName", "companyNo", "id", "orgName", "realName", "roleId",
+                                            "roleName", "roleType", "token"], do_mysql(sql, user)[0]))
             expect = do_re.sub(value.temp, "{}".format(expect_data))
         else:
             expect = value.temp
@@ -80,7 +79,8 @@ class TestLogin(unittest.TestCase):
         # Parse http response,then gain token's value
         try:
             token = json.loads(res.text)["data"]["token"]
-            setattr(GetToken, "token", token)
+            if token:
+                setattr(GetToken, "token", token)
         except Exception as e:
             pass
 
@@ -96,6 +96,7 @@ class TestLogin(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    one_suite = unittest.TestSuite(map(TestLogin, ['test_login']))
     unittest.main()
 
 
