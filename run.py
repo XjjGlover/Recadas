d37c6f0a4f1bea65f0b672aaeaac5990b763d0a0
xujiajun <924207089@qq.com>
2020-07-03 15:27:09 +0800

    Alter enviroment-variable path.

--- run.py
+++ run.py
@@ -7,16 +7,15 @@
 # @Software: PyCharm
 # @contact: 924207089@qq.com
 
-
-import unittest
 import os,sys
-from case.login import TestLogin
-from util.get_data import GetToken
-
 # Add enviroment-virable.
 dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(dir)
 sys.path.append(r"C:\Users\86188\AppData\Local\Programs\Python\Python37\Lib\site-packages")
+
+import unittest
+from case.login import TestLogin
+from util.get_data import GetToken
 
 # result = unittest.TextTestResult(sys.stdout, 'test result', 1)
 suite = unittest.TestSuite()
