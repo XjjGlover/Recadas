21b3c8644995ac535d02cb2d7796f8d90aa6c6b6
xujiajun <924207089@qq.com>
2020-07-03 15:17:34 +0800

    Add enviroment-variable in run.py.

--- run.py
+++ run.py
@@ -9,10 +9,14 @@
 
 
 import unittest
-import time
-import sys
+import os,sys
 from case.login import TestLogin
 from util.get_data import GetToken
+
+# Add enviroment-virable.
+dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(dir)
+sys.path.append(r"C:\Users\86188\AppData\Local\Programs\Python\Python37\Lib\site-packages\unittest2-0.5.0-py3.7.egg")
 
 # result = unittest.TextTestResult(sys.stdout, 'test result', 1)
 suite = unittest.TestSuite()
