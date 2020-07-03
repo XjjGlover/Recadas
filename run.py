0af940807d115fee3f5330619d38a2ec9e007085
xujiajun <924207089@qq.com>
2020-07-03 15:21:06 +0800

    Alter enviroment-variable path.

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ from util.get_data import GetToken
 # Add enviroment-virable.
 dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(dir)
-sys.path.append(r"C:\Users\86188\AppData\Local\Programs\Python\Python37\Lib\site-packages\unittest2-0.5.0-py3.7.egg")
+sys.path.append(r"C:\Users\86188\AppData\Local\Programs\Python\Python37\Lib\site-packages")
 
 # result = unittest.TextTestResult(sys.stdout, 'test result', 1)
 suite = unittest.TestSuite()
